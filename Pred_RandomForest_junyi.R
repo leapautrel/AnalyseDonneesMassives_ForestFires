33c57e237aec68dbd7d868d5b29b1979e2b1899d
Junyi Zhao <junyi.zhao5@gmail.com>
2020-10-12 20:54:41 +0200

    Update Pred_RandomForest_junyi.R

--- Pred_RandomForest_junyi.R
+++ Pred_RandomForest_junyi.R
@@ -7,6 +7,8 @@ library(ggplot2) # graphiques
 library(randomForest) # prediction
 library(rpart) #prédiction CART
 
+
+
 # Set Working Directory ----
 #setwd("D:/Google Drive/Agrocampus/M2/UE4-AnalyseDonneesMassiveR/Projet_Foret") # Direction fichier  GitHub Lea
 setwd("C:/Users/mimi/Desktop/M2/Analyse de données massives/projet") # Direction fichier Junyi
