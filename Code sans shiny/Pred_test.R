cfab7cc1a2f0e96ab85f2352a28751dfaf101aa5
Léa Pautrel <pautrel.lea@gmail.com>
2020-10-14 20:43:14 +0200

    Update Pred_test.R

--- Code sans shiny/Pred_test.R
+++ Code sans shiny/Pred_test.R
@@ -32,85 +32,30 @@ data_train <- as.data.table(fires_selec[train_index, c(1:4, 6)])
 data_test <- as.data.table(fires_selec[test_index, c(1:4, 6)])
 print(object.size(data_train), units = 'Mb')
 
-# On initalise les vecteurs
-ntree <- c(1, 5, 10, 25, 50)
-accuracy <- rep(0, 5)
-syst_time <- rep(0, 5)
-
-# 1 tree
-mod_rf_1 <- randomForest(stat_cause_descr ~ .,
-														 ntree = 1,
-														 data = data_train)
-p_rf_1 <- predict(mod_rf_1,
-											newdata = data_test,
-											type = "response")
-cM_1 <- caret::confusionMatrix(factor(p_rf_1, 
-																		levels = levels(data_test$stat_cause_descr)), 
-														 reference = data_test$stat_cause_descr)
-accuracy[1] <- cM_1$overall["Accuracy"]
-syst_time[1] <- system.time(randomForest(stat_cause_descr ~ .,
-																			 ntree = 1,
-																			 data = data_train))[3]
-
-# 5 trees
-mod_rf_5 <- randomForest(stat_cause_descr ~ .,
-												 ntree = 5,
+# Fonction pour random forest selon ntree ----
+rf_ntrees <- function(ntree) {
+	mod_rf <- randomForest(stat_cause_descr ~ .,
+												 ntree = ntree,
 												 data = data_train)
-p_rf_5 <- predict(mod_rf_5,
+	p_rf <- predict(mod_rf,
 									newdata = data_test,
 									type = "response")
-cM_5 <- caret::confusionMatrix(factor(p_rf_5, 
-																		levels = levels(data_test$stat_cause_descr)), 
-														 reference = data_test$stat_cause_descr)
-accuracy[2] <- cM_5$overall["Accuracy"]
-syst_time[2] <- system.time(randomForest(stat_cause_descr ~ .,
-																				 ntree = 5,
-																				 data = data_train))[3]
-# 10 trees
-mod_rf_10 <- randomForest(stat_cause_descr ~ .,
-												 ntree = 10,
-												 data = data_train)
-p_rf_10 <- predict(mod_rf_10,
-									newdata = data_test,
-									type = "response")
-cM_10 <- caret::confusionMatrix(factor(p_rf_10, 
-																			levels = levels(data_test$stat_cause_descr)), 
+	cM <- caret::confusionMatrix(factor(p_rf,
+																			levels = levels(data_test$stat_cause_descr)),
 															 reference = data_test$stat_cause_descr)
-accuracy[3] <- cM_10$overall["Accuracy"]
-syst_time[3] <- system.time(randomForest(stat_cause_descr ~ .,
-																				 ntree = 10,
-																				 data = data_train))[3]
+	accuracy <- cM$overall["Accuracy"]
+	syst_time <- system.time(randomForest(stat_cause_descr ~ .,
+																				ntree = ntree,
+																				data = data_train))[3]
+	list(ntree = ntree, accuracy = accuracy, syst_time = syst_time)
+}
 
-# 25 trees 
-mod_rf_25 <- randomForest(stat_cause_descr ~ .,
-												 ntree = 25,
-												 data = data_train)
-p_rf_25 <- predict(mod_rf_25,
-									newdata = data_test,
-									type = "response")
-cM_25 <- caret::confusionMatrix(factor(p_rf_25, 
-																			levels = levels(data_test$stat_cause_descr)), 
-															 reference = data_test$stat_cause_descr)
-accuracy[4] <- cM_25$overall["Accuracy"]
-syst_time[4] <- system.time(randomForest(stat_cause_descr ~ .,
-																				 ntree = 25,
-																				 data = data_train))[3]
+# Application fonction ----
+vec_ntree <- c(1, 2, 5, 10, 25, 50, 75)
+res <- sapply(vec_ntree, FUN = rf_ntrees)
+res
 
-# 50 trees
-mod_rf_50 <- randomForest(stat_cause_descr ~ .,
-													ntree = 50,
-													data = data_train)
-p_rf_50 <- predict(mod_rf_50,
-									 newdata = data_test,
-									 type = "response")
-cM_50 <- caret::confusionMatrix(factor(p_rf_50, 
-																			 levels = levels(data_test$stat_cause_descr)), 
-																reference = data_test$stat_cause_descr)
-accuracy[5] <- cM_50$overall["Accuracy"]
-syst_time[5] <- system.time(randomForest(stat_cause_descr ~ .,
-																				 ntree = 50,
-																				 data = data_train))[3]
-
+# CA C'EST PLUS BON, a réadapter avec la fonction
 data_plot <- data.frame(ntree = ntree,
 												accuracy = accuracy, 
 												syst_time = syst_time)
